cea831a3286f62e1c641cf4dc8de78fcc6705380
Thòng Hoài Quý <hoaiquy33@gmail.com>
2026-07-09 09:58:33 +0700

    fix: backend

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -48,6 +48,6 @@ export async function apiFetch(url, options = {}) {
     'Content-Type': 'application/json',
     ...(options.headers || {}),
   }
-  if (token) headers['Authorization'] = token
+  if (token) headers['Authorization'] = `Bearer ${token}`
   return fetch(url, { ...options, headers })
 }
